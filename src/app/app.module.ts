0cf7490e2ddceb82a01f6865d4e74f6ad71831af
Andre de Camargo <camargo_and@hotmail.com>
2023-07-03 14:55:34 -0300

    criado os Componete de imput e o formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MaterialModule } from './shared/material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastroFilmeComponent } from './filmes/cadastro-filme/cadastro-filme.component';
+import { FormsModule } from '@angular/forms';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { TopoComponent } from './shared/components/topo/topo.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastroFilmeComponent,
+    TopoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    LayoutModule,
+    MaterialModule
   ],
-  providers: [],
+  entryComponents: [],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/filmes/cadastro-filme/cadastro-filme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-filme',
+  templateUrl: './cadastro-filme.component.html',
+  styleUrls: ['./cadastro-filme.component.css']
+})
+export class CadastroFilmeComponent {
+
+}
